71f607f685319a36463d996e77d95af410f36e5d
Matthew Muriithi <muriithimatthew@gmail.com>
2024-04-01 21:33:51 -0400

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,195 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        
-        </a>
+    <div id='__next'>
+      <div className='__variable_20b187 group/spotlight relative'>
+        <div
+          className='pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute'
+          style={{
+            background:
+              "radial-gradient(600px at 151px 2739px, rgba(29, 78, 216, 0.15), transparent 80%)",
+          }}
+        ></div>
+        <div className='mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:py-0'>
+          <a
+            href='#content'
+            className='absolute left-0 top-o block -translate-x-full rounded bg-gradient-to-br from-teal-400 via-blue-500 to-purple-600 px-4 py-3 text-sm font-bold uppercase tracking-widest text-white focus-visible:translate-x-0'
+          >
+            Skip to Content
+          </a>
+          <div
+            id='two-halves-seperator'
+            className='lg:flex lg:justify-between lg:gap-4'
+          >
+            <header className='lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24'>
+              <div>
+                <h1 className='text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl'>
+                  <a href='/'>Matthew Muriithi</a>
+                </h1>
+                <h2 className='mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl'>
+                  Software Engineer
+                </h2>
+                <p className='mt-4 max-w-xs leading-normal'>
+                  I build models and applications that use those models
+                </p>
+                <nav
+                  className='nav hidden lg:block'
+                  aria-label='In-page jump links'
+                >
+                  <ul className='mt-16 w-max'>
+                    <li>
+                      <a className='group flex items-center py-3' href='#about'>
+                        <span className='nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none'></span>
+                        <span className='nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200'>
+                          About
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className='group flex items-center py-3'
+                        href='#experience'
+                      >
+                        <span className='nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none'></span>
+                        <span className='nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200'>
+                          Experience
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className='group flex items-center py-3 active'
+                        href='#blog'
+                      >
+                        <span className='nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none'></span>
+                        <span className='nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200'>
+                          Blog
+                        </span>
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+              <ul
+                className='ml-1 mt-8 flex items-center'
+                aria-label='Social media'
+              >
+                <li className='mr-5 text-xs shrink-0'>
+                  <a
+                    className='block hover:text-slate-200'
+                    href='github.com'
+                    target='_blank'
+                    rel='noreferrer noopener'
+                    aria-label='Github (opens in a new tab)'
+                    title='Github'
+                  >
+                    {/* noopener to prevent new page from accessing source page window object noferrer for privacy*/}
+                    <span className='sr-only'>Github</span>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 16 16'
+                      fill='currentColor'
+                      className='w-6 h-6'
+                      aria-hidden={true}
+                    >
+                      <path d='M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z'></path>
+                    </svg>
+                  </a>
+                </li>
+                <li className='mr-5 text-xs shrink-0'>
+                  <a
+                    className='block hover:text-slate-200'
+                    href='https://www.linkedin.com/in/bchiang7/'
+                    target='_blank'
+                    rel='noreferrer noopener'
+                    aria-label='LinkedIn (opens in a new tab)'
+                    title='LinkedIn'
+                  >
+                    <span className='sr-only'>LinkedIn</span>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 24 24'
+                      fill='currentColor'
+                      className='h-6 w-6'
+                      aria-hidden='true'
+                    >
+                      <path d='M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z'></path>
+                    </svg>
+                  </a>
+                </li>
+                <li className='mr-5 text-xs shrink-0'>
+                  <a
+                    className='block hover:text-slate-200'
+                    href='https://instagram.com/bchiang7'
+                    target='_blank'
+                    rel='noreferrer noopener'
+                    aria-label='Instagram (opens in a new tab)'
+                    title='Instagram'
+                  >
+                    <span className='sr-only'>Instagram</span>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 1000 1000'
+                      fill='currentColor'
+                      className='h-6 w-6'
+                      aria-hidden='true'
+                    >
+                      <path d='M295.42,6c-53.2,2.51-89.53,11-121.29,23.48-32.87,12.81-60.73,30-88.45,57.82S40.89,143,28.17,175.92c-12.31,31.83-20.65,68.19-23,121.42S2.3,367.68,2.56,503.46,3.42,656.26,6,709.6c2.54,53.19,11,89.51,23.48,121.28,12.83,32.87,30,60.72,57.83,88.45S143,964.09,176,976.83c31.8,12.29,68.17,20.67,121.39,23s70.35,2.87,206.09,2.61,152.83-.86,206.16-3.39S799.1,988,830.88,975.58c32.87-12.86,60.74-30,88.45-57.84S964.1,862,976.81,829.06c12.32-31.8,20.69-68.17,23-121.35,2.33-53.37,2.88-70.41,2.62-206.17s-.87-152.78-3.4-206.1-11-89.53-23.47-121.32c-12.85-32.87-30-60.7-57.82-88.45S862,40.87,829.07,28.19c-31.82-12.31-68.17-20.7-121.39-23S637.33,2.3,501.54,2.56,348.75,3.4,295.42,6m5.84,903.88c-48.75-2.12-75.22-10.22-92.86-17-23.36-9-40-19.88-57.58-37.29s-28.38-34.11-37.5-57.42c-6.85-17.64-15.1-44.08-17.38-92.83-2.48-52.69-3-68.51-3.29-202s.22-149.29,2.53-202c2.08-48.71,10.23-75.21,17-92.84,9-23.39,19.84-40,37.29-57.57s34.1-28.39,57.43-37.51c17.62-6.88,44.06-15.06,92.79-17.38,52.73-2.5,68.53-3,202-3.29s149.31.21,202.06,2.53c48.71,2.12,75.22,10.19,92.83,17,23.37,9,40,19.81,57.57,37.29s28.4,34.07,37.52,57.45c6.89,17.57,15.07,44,17.37,92.76,2.51,52.73,3.08,68.54,3.32,202s-.23,149.31-2.54,202c-2.13,48.75-10.21,75.23-17,92.89-9,23.35-19.85,40-37.31,57.56s-34.09,28.38-57.43,37.5c-17.6,6.87-44.07,15.07-92.76,17.39-52.73,2.48-68.53,3-202.05,3.29s-149.27-.25-202-2.53m407.6-674.61a60,60,0,1,0,59.88-60.1,60,60,0,0,0-59.88,60.1M245.77,503c.28,141.8,115.44,256.49,257.21,256.22S759.52,643.8,759.25,502,643.79,245.48,502,245.76,245.5,361.22,245.77,503m90.06-.18a166.67,166.67,0,1,1,167,166.34,166.65,166.65,0,0,1-167-166.34'></path>
+                    </svg>
+                  </a>
+                </li>{" "}
+                <li className='mr-5 text-xs shrink-0'>
+                  <a
+                    className='block hover:text-slate-200'
+                    href='https://twitter.com/bchiang7'
+                    target='_blank'
+                    rel='noreferrer noopener'
+                    aria-label='Twitter (opens in a new tab)'
+                    title='Twitter'
+                  >
+                    <span className='sr-only'>Twitter</span>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 1200 1227'
+                      fill='none'
+                      className='h-5 w-5'
+                      aria-hidden='true'
+                    >
+                      <path
+                        d='M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z'
+                        fill='currentColor'
+                      ></path>
+                    </svg>
+                  </a>
+                </li>
+              </ul>
+            </header>
+            <main>
+              <h1 className='uppercase text-orange-900'>Vite + React</h1>
+              <div className='card'>
+                <button onClick={() => setCount((count) => count + 1)}>
+                  count is {count}
+                </button>
+                <p>
+                  Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+              </div>
+              <p className='read-the-docs'>
+                Click on the Vite and React logos to learn more
+              </p>
+            </main>
+          </div>
+        </div>
       </div>
-      <h1 className="uppercase text-orange-900">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
